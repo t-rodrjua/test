c5221a035f432b86a2915bf78193e4897d6f9446
t-rodrjua <juan.pablo.rodriguez@autodesk.com>
2022-01-28 19:32:22 -0500

    uno

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hola mundo</div>;
+  return (
+    <div className="App">
+      <div>1</div>
+    </div>
+  );
 }
 
 export default App;
